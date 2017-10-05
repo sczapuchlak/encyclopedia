53ade964923e74686e8df13c7388a39a0e609ff3
FreeMasen <RFM@Herman.local>
2017-10-04 20:34:11 -0500

    small fix

--- src/login.py
+++ src/login.py
@@ -7,22 +7,19 @@ class UserManager():
     '''Manager the user's of this application'''
     def __init__(self):
         self.database = Database()
-        self.user = user()
 
     def add_user(self, first_name, last_name, username, password):
         '''Add a user to the database'''
         password_hash = self._hash_password(password)
         self.database.add_user(first_name, last_name, username, password_hash)
 
-    def findUser(self, first_name, last_name, username, password, email):
+    def find_user(self, first_name, last_name, username, password, email):
         '''looks for username and email'''
         user = self.database.get_user(username)
         mail = self.database.get_user(email)
-
         if user is not None:
-            return self.user
-        if mail is not None:
-            return self.mail
+            return user
+        return mail
 
     def validate_credentials(self, username, password):
         '''validate a password matches what is in the database'''
