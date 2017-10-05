9cbaa20b9afc8e9228a6cc8672b3aa1aada7ba31
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-10-04 23:32:35 -0500

    Started practicing with sqlalchemy. Unsure if we are still going this route. Also readded sqlite3 database to the gitignore file. Is not in current version for some reason.

--- src/ORM.py
+++ src/ORM.py
@@ -17,3 +17,21 @@ class User(Base):
     def __repr__(self):
         return "<User(FirstName='%s', LastName='%s', UserName='%s', Password='%s')>" % (
             self.FirstName, self.LastName, self.UserName, self.Password)
+
+Base.metadata.create_all(engine)
+ed_user = User(FirstName='Alex', LastName='Simpson', UserName='asimpson440', Password='password')
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+Session.configure(bind=engine)
+session = Session()
+session.add(ed_user)
+our_user = session.query(User).filter_by(FirstName='Alex').first()
+session.add_all([
+    User(FirstName='Tom', LastName='Harry', UserName='tharry123', Password='123456'),
+    User(FirstName='Jen', LastName='Caron', UserName='jcaron456', Password='ajkldas'),
+    User(FirstName='Brady', LastName='Candy', UserName='bcandy789', Password='asdf')])
+ed_user.Password = "alotoftime"
+print(session.new)
+session.commit()
+print(ed_user.UserID)
