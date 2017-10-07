4a0eef94c5b9c67a4b1cfe8410eb7e6f07995bbf
Alexander Simpson <alexsimpson440dev@gmail.com>
2017-10-07 11:48:26 -0500

    Added new file for user search table. Entered the Data Model into new file.

--- src/user.py
+++ src/user.py
@@ -1,7 +1,7 @@
-class user:
+class User:
     def __init__(self, firstName, lastName, userName, password, email):
         self.firstName = firstName
         self.lastName = lastName
+        self.email = email
         self.userName = userName
         self.password = password
-        self.email = email
---
+++ src/userSearch.py
@@ -0,0 +1,5 @@
+class UserSearch():
+    def __init__(self, searchId, searchText, userId):
+        self.searchId = searchId
+        self.searchText = searchText
+        self.userId = userId
