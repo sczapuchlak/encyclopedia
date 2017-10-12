7b056a23441e7023ba6cb6c96e92169cab9caf50
FreeMasen <RFM@Herman.local>
2017-10-11 21:08:02 -0500

    Minor tweeks to include searchs and services in tables

--- src/database.py
+++ src/database.py
@@ -3,17 +3,21 @@ import sqlite3
 from src.user import User
 from src.userSearch import UserSearch
 from sqlalchemy import Table, MetaData, Column, Integer, String, ForeignKey, create_engine
-from sqlalchemy.orm import mapper, sessionmaker
-METADATA = MetaData()
+import sqlalchemy.types as types
+from sqlalchemy.orm import mapper, sessionmaker, relationship
 #database class
 class Database():
     '''Data Access Layer'''
     def __init__(self, connection_string='sqlite:///media.cheetah.sqlite3'):
         self.sql_file = connection_string
-        self.users = self._map_users()
-        self.user_searches = self._map_user_searches()
         self.engine = self._get_connection()
-        METADATA.create_all(self.engine)
+        self.metadata = MetaData(bind=self.engine)
+        try:
+            self.users, self.user_searches = self._create_tables()
+            self._map_user_searches()
+            self._map_users()
+        except Exception as e:
+            print(e)
         #old: self._try_create_tables()
 
 #-------------------------------------NEW CODE FOR ORM-------------------------------------------
@@ -29,30 +33,68 @@ class Database():
     def find_user_with_email(self, email):
         session = self._get_session()
         for user in session.query(User).\
-            filter(User.user_name==email):
+            filter(User.email_address==email):
             return user
-    def _map_users(self):
-        users = Table('Users', METADATA,\
+    def add_search(self, search):
+        session = self._get_session()
+        session.add(search)
+        session.commit()
+    def get_searches_for_user(self, user_id):
+        session = self._get_session()
+        return session.query(UserSearch).filter(UserSearch.user_id == user_id).all()
+    def _create_tables(self):
+        users = Table('Users', self.metadata,\
             Column('user_id', Integer, primary_key=True),
             Column('first_name', String),\
             Column('last_name', String),\
             Column('user_name', String),\
             Column('password', String),\
             Column('email_address', String))
-        mapper(User, users)
-        return users
-    def _map_user_searches(self):
-        user_searches = Table('UserSearches', METADATA,\
+        users.create(self.engine, checkfirst=True)
+        user_searches = Table('UserSearches', self.metadata,\
             Column('search_id', Integer, primary_key=True),
             Column('search_text', String),\
-            Column('user_id', Integer))
-        mapper(UserSearch, user_searches)
-        return user_searches
+            Column('services', Services),\
+            Column('user_id', Integer, ForeignKey('Users')))
+        user_searches.create(self.engine, checkfirst=True)
+        return (users, user_searches)
+    def _map_users(self):
+        mapper(User, self.users, properties={
+            'searches': relationship(UserSearch, backref="Users")
+        })
+    def _map_user_searches(self):
+        mapper(UserSearch, self.user_searches)
     def _get_connection(self):
         engine = create_engine(self.sql_file)
         return engine
     def _get_session(self):
         Session = sessionmaker(bind=self.engine)
         return Session()
+
+
+class Services(types.TypeDecorator):
+    impl = types.Integer
+
+    def process_bind_param(self, value, dialect):
+        ret = 0
+        for service in value:
+            if service.lower() == 'twitter':
+                ret += 1
+            if service.lower() == 'giphy':
+                ret += 2
+            if service.lower() == 'flickr':
+                ret += 4
+        return ret
+    def process_result_value(self, value, dialect):
+        ret = list()
+        if value & 1 > 0:
+            ret.append('twitter')
+        if value & 2 > 0:
+            ret.append('giphy')
+        if value & 4 > 0:
+            ret.append('flickr')
+        return ret
+
 if __name__ == '__main__':
     _ = Database()
+
--- src/user.py
+++ src/user.py
@@ -1,12 +1,13 @@
 '''Represents a user of our application'''
 class User(object):
-    def __init__(self, first_name, last_name, user_name, email_address, password, user_id=None):
+    def __init__(self, first_name, last_name, user_name, email_address, password,searches=list(), user_id=None):
         self.first_name = first_name
         self.last_name = last_name
         self.user_name = user_name
         self.email_address = email_address
         self.password = password
         self.user_id = user_id
+        self.searches = searches
     def __repr__(self):
         return '{id}:{un}\n----------\n{fn} {ln}\n{e}\n{p}'\
         .format(id=self.user_id, un=self.user_name, fn=self.first_name, \
