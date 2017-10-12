02ee38a3f9406e669996f87a7a36283a14afd13c
FreeMasen <RFM@Herman.local>
2017-10-11 22:20:27 -0500

    loosened route tests, added database trys

--- tests/routes_test.py
+++ tests/routes_test.py
@@ -21,17 +21,17 @@ class RoutesTest(unittest.TestCase):
         sign_up_page = self.app.get('/signup.html')
         self.assertEqual(sign_up_page.status_code, 200)
         sign_up_response = self.app.post('/signup', data=dict(username='username', password='password', firstname='first', lastname='last', email='e@mail.co'))
-        self.assertEqual(sign_up_response.status_code, 200)
+        self.assertIn(sign_up_response.status_code, [302, 200])
     def test_sign_out(self):
         print('RoutesTest.test_sign_out')
         logout = self.app.get('/signout')
-        self.assertEqual(logout.status_code, 302)
+        self.assertIn(logout.status_code, [302, 200])
     def test_login(self):
         print('RoutesTest.test_login')
         login_page = self.app.get('/login.html')
         self.assertEqual(login_page.status_code, 200)
         login_response = self.app.post('/login', data=dict(username='username', password='password'))
-        self.assertEqual(login_response.status_code, 302)
+        self.assertIn(login_response.status_code, [302, 200])
     def test_home(self):
         print('RoutesTest.test_home')
         pass
